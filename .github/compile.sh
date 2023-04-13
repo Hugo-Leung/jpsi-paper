cfe5abb6c054ba1f76c4bfd667ec68bcec36f6c1
Ching Him Leung <himhim2001@live.hk>
2023-04-12 19:07:12 -0500

    Initial commit

---
+++ .github/compile.sh
@@ -0,0 +1,23 @@
+current_tag=$1
+prev_tag=$2
+
+MAIN="main"
+latex_option=""
+outName=""
+if [[ "${current_tag}" =~ refs/tags/* ]]
+then
+	outName="DIS_chleung_${current_tag:10}"
+fi
+
+latexmk ${latex_option} -outdir=build ${MAIN}
+
+if [ -n "${prev_tag}" ]; then
+	latexdiff-vc --git -r ${prev_tag} ${MAIN}.tex  --flatten
+	diffName=${MAIN}-diff${prev_tag}
+	echo ${diffName}
+	latexmk ${latex_option} -outdir=build ${diffName}
+fi
+
+if [ -n "${outName}" ]; then
+	mv build/${MAIN}.pdf build/$outName.pdf
+fi
