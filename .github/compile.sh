df77f819fc4115570634903bae0951b7350bbad2
Ching Him Leung <himhim2001@live.hk>
2023-04-19 18:43:45 -0500

    github action

---
+++ .github/compile.sh
@@ -0,0 +1,25 @@
+current_tag=$1
+prev_tag=$2
+
+MAIN="jpsi"
+latex_option=""
+outName=""
+if [[ "${current_tag}" =~ refs/tags/* ]]
+then
+	outName="jpsi_${current_tag:10}"
+else
+	outName="jpsi_${current_tag}"
+fi
+
+latexmk ${latex_option} -outdir=build ${MAIN}
+
+if [ -n "${prev_tag}" ]; then
+	latexdiff-vc --git -r ${prev_tag} ${MAIN}.tex  --flatten
+	diffName=${MAIN}-diff${prev_tag}
+	echo ${diffName}
+	latexmk ${latex_option} -outdir=build ${diffName}
+fi
+
+if [ -n "${outName}" ]; then
+	mv build/${MAIN}.pdf build/$outName.pdf
+fi
